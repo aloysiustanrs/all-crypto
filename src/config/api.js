94d905db000a5d7854c8700652076ba839919ce1
aloysiustanrs <aloysiustanrs@gmail.com>
2022-05-22 11:45:08 +0800

    UPLOAD

--- src/config/api.js
+++ src/config/api.js
@@ -15,4 +15,4 @@ export const GlobalStats = () => `https://api.coingecko.com/api/v3/global`;
 export const ExchangesList = () => `https://api.coingecko.com/api/v3/exchanges`;
 
 export const NewsList = () =>
-  `https://gnews.io/api/v4/search?q=crypto&country=us&language=en&token=${process.env.REACT_APP_API_KEY}`;
+  `https://gnews.io/api/v4/search?q=crypto&country=us&language=en&token=${process.env.REACT_APP_GNEWS}`;
